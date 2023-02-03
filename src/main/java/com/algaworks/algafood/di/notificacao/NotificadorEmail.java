394f17fd69c5658f2d300a1ca9dc2d5bde19cbfb
Wenderson Santos <guto15santos@gmail.com>
2023-02-03 09:29:14 -0300

    2.25. Criando e acessando propriedades customizadas com @Value

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,15 +1,16 @@
 package com.algaworks.algafood.di.notificacao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 
 @Component
-@Profile(value = "prod")
 @TipoDoNotificador(NivelUrgencia.NORMAL)
+@Profile(value = "prod")
 public class NotificadorEmail implements Notificador {
-	
+
 	@Override
 	public void notificar(Cliente c, String m) {
 		System.out.printf("Notificando %s por SMS através do telefone %s : %s\n",
--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorSMS.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorSMS.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.di.notificacao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
@@ -8,9 +9,15 @@ import com.algaworks.algafood.di.modelo.Cliente;
 @TipoDoNotificador(NivelUrgencia.URGENTE)
 public class NotificadorSMS implements Notificador {
 	
+	
+	@Value("${notificador.email.smtp}")
+	private String hostsmtp;
+	
+	
 	@Override
 	public void notificar(Cliente c, String m) {
 		System.out.printf("Notificando %s através do email %s : %s\n", c.getNome(), c.getEmail(), m);
+		System.out.println(hostsmtp);
 	}
 	
 }
