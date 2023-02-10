a6ae0f39f88a23c91336c4e56977eb2cd1ff2d06
Wenderson Santos <guto15santos@gmail.com>
2023-02-10 20:10:08 -0300

    4.20. Manipulando a resposta HTTP com ResponseEntity

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -1,14 +1,12 @@
 package com.algaworks.algafood.api.controller;
 
-import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,18 +17,19 @@ public class CozinhaController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<Cozinha> listar(){
         return cozinhaRepository.todas();
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-    public CozinhasXmlWrapper listarXml(){
-        return new CozinhasXmlWrapper(cozinhaRepository.todas());
-    }
-
     @GetMapping("/{cozinhaId}")
-    public Cozinha buscar(@PathVariable Long cozinhaId){
-        return cozinhaRepository.porId(cozinhaId);
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId){
+        Cozinha cozinha = cozinhaRepository.porId(cozinhaId);
+
+        HttpHeaders header = new HttpHeaders();
+        header.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
+
+        return ResponseEntity.status(HttpStatus.FOUND).headers(header).body(cozinha);
     }
 }
