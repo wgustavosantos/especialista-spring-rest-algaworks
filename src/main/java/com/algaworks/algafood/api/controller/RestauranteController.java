39cbd258f28f1135f306033575ac0c3797e8a887
Wenderson Santos <guto15santos@gmail.com>
2023-02-14 09:50:22 -0300

    4.34. Finalizando a atualização parcial com a API de Reflections do Spring

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -4,11 +4,14 @@ import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.RestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
@@ -80,9 +83,20 @@ public class RestauranteController {
         return atualizar(restauranteAtual, id);
     }
 
-    private static void merge(Map<String, Object> campos, Restaurante restauranteDestino) {
-        campos.forEach((nomePropriedade, valorPropriedade) -> {
+    private static void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+        dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            final Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+
             System.out.println(nomePropriedade + " = " + valorPropriedade);
+
+            Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+            field.setAccessible(true);
+
+            final Object novoValorConvertido = ReflectionUtils.getField(field, restauranteOrigem);
+
+            ReflectionUtils.setField(field, restauranteDestino, novoValorConvertido);
         });
     }
 }
