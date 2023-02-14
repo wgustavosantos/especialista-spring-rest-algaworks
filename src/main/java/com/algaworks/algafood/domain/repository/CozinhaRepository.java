444b8583f1e8a5d14d0199a228fa0905074875e3
Wenderson Santos <guto15santos@gmail.com>
2023-02-14 15:23:10 -0300

    5.3. Criando um repositório com Spring Data JPA (SDJ)

--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -1,13 +1,13 @@
 package com.algaworks.algafood.domain.repository;
 
 import com.algaworks.algafood.domain.model.Cozinha;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CozinhaRepository {
+public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
     List<Cozinha> todas();
     List<Cozinha> consultarPorNome(String nome);
-
     Cozinha buscar(Long id);
     Cozinha salvar(Cozinha cozinha);
     void excluir(Long id);
