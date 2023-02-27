8229eec487c29d4a955768f8d43f97aa26d3ebc4
Wenderson Santos <guto15santos@gmail.com>
2023-02-27 15:56:59 -0300

    8.18. Padronizando o formato de problemas no corpo de respostas com a RFC 7807

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -11,14 +11,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDateTime;
-
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntidadeNaoEncontradaException.class) /*também subclasses*/
     public ResponseEntity<?> handleEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex, WebRequest request) {
-        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+
+        HttpStatus statusNotFound = HttpStatus.NOT_FOUND;
+        ProblemType problemType = ProblemType.ENTIDADE_NAO_ENCONTRADA;
+        final Problem problem2 = createProblemType(statusNotFound, problemType, ex.getMessage()).build();
+//        Problem problem = Problem.builder()
+//                .status(statusNotFound.value())
+//                .type("http://localhost:8080/entidade-nao-encontrada")
+//                .title("Entidade não encontrada")
+//                .detail(ex.getMessage())
+//                .build();
+
+        return handleExceptionInternal(ex, problem2, new HttpHeaders(), statusNotFound, request);
 
     }
 
@@ -37,11 +46,27 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
                                                              HttpStatus status, WebRequest request) {
 
-            body = Error.builder()
-                    .dataHora(LocalDateTime.now())
-                    .mensagem(body instanceof String ? (String) body: status.getReasonPhrase())
+        /**
+         * se o (Object) body não for nullo, ele é do tipo Problem (Especificação RFC 7807) e não é necessário
+         * criar um novo objeto.
+         * Se ele for nullo, a exceção foi tratada pelo SpringMVC interno, o corpo da exceção vem nulla.
+         * Se ele for apenas uma String também um problem vai ser instanciado e construído a partir da RFC 7807
+         **/
+        if(body == null || body instanceof String) {
+            body = Problem.builder()
+                    .title(body instanceof String ? (String) body : status.getReasonPhrase())
+                    .status(status.value())
                     .build();
-            
+        }
         return super.handleExceptionInternal(ex, body, headers, status, request);
+    }
+
+    private Problem.ProblemBuilder createProblemType (HttpStatus status, ProblemType problemType, String detail){
+
+        return Problem.builder()
+                .status(status.value())
+                .type(problemType.getUri())
+                .title(problemType.getTitle())
+                .detail(detail);
     }
 }
