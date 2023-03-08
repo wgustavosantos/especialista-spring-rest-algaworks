e8d392944bd2f9e18f82aacd0ac818acb03759fe
Wenderson Santos <guto15santos@gmail.com>
2023-03-08 11:00:28 -0300

    10.4 - 10.5. Desafio- escrevendo testes de integração

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
@@ -1,7 +1,13 @@
 package com.algaworks.algafood;
 
+import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.service.CozinhaService;
+import com.algaworks.algafood.domain.service.RestauranteService;
+import org.assertj.core.api.AbstractThrowableAssert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +15,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.validation.ConstraintViolationException;
 
+import java.util.function.Predicate;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 class CadastroCozinhaIntegrationTests {
@@ -17,9 +26,12 @@ class CadastroCozinhaIntegrationTests {
     @Autowired
     private CozinhaService cozinhaService;
 
+    @Autowired
+    private RestauranteService restauranteService;
+
     @Test
     /*Happy Path*/
-    public void testarCadastroCozinhaComSucesso(){
+    public void testarCadastroCozinhaComSucesso() {
         // cenário
         Cozinha novaCozinha = new Cozinha();
         novaCozinha.setNome("Chinesa");
@@ -34,7 +46,7 @@ class CadastroCozinhaIntegrationTests {
 
     @Test
     /*bad path*/
-    public void testarCadastroCozinhaSemNome(){
+    public void testarCadastroCozinhaSemNome() {
         // cenário
         Cozinha novaCozinha = new Cozinha();
         novaCozinha.setNome(null);
@@ -43,4 +55,26 @@ class CadastroCozinhaIntegrationTests {
         Assertions.assertThrows(ConstraintViolationException.class,
                 () -> cozinhaService.salvar(novaCozinha));
     }
+
+    @Test
+    /* bad path */
+    public void deveFalhar_QuandoExcluirCozinhaEmUso() {
+        final Long cozinhaId = restauranteService.buscarOuFalhar(1L).getCozinha().getId();
+
+        final EntidadeEmUsoException erroEsperado = Assertions.
+                assertThrows(EntidadeEmUsoException.class, () -> cozinhaService.deletar(cozinhaId));
+        assertThat(erroEsperado).isNotNull();
+    }
+
+        @Test
+        /* bad path */
+        public void deveFalhar_QuandoExcluirCozinhaInexistente() {
+            final Long cozinhaId = 55L;
+
+            final ConstraintViolationException erroEsperado = Assertions
+                    .assertThrows(ConstraintViolationException.class, () -> cozinhaService.deletar(cozinhaId));
+            System.out.println("antes do assertThaht");
+
+            assertThat(erroEsperado).isNotNull();
+        }
 }
