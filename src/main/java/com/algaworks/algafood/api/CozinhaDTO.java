95085ec351ef0a060c6110984312eeca2cc28d59
Wenderson Santos <guto15santos@gmail.com>
2023-03-13 13:05:39 -0300

    11.9. Isolando o Domain Model do Representation Model com o padrão DTO

---
+++ src/main/java/com/algaworks/algafood/api/CozinhaDTO.java
@@ -0,0 +1,11 @@
+package com.algaworks.algafood.api;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CozinhaDTO {
+    private Long id;
+    private String nome;
+}
---
+++ src/main/java/com/algaworks/algafood/api/RestauranteDTO.java
@@ -0,0 +1,10 @@
+package com.algaworks.algafood.api;
+
+import java.math.BigDecimal;
+
+public class RestauranteDTO {
+    private Long id;
+    private String nome;
+    private BigDecimal taxaFrete;
+    private CozinhaDTO cozinha;
+}
