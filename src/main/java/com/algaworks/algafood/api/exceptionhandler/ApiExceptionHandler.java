0b85f6da0da969c103e2bcfc0b3879f033470c56
Wenderson Santos <guto15santos@gmail.com>
2023-03-31 16:19:22 -0300

    13.7. Tratando BindException ao enviar parâmetros de URL inválidos

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -18,6 +18,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -98,7 +99,6 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
      * @param headers the headers to be written to the response
      * @param status the selected response status.
      * @param request the current request
-     * @return
      */
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers,
@@ -212,6 +212,11 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         return handleValidationInternal(ex, ex.getBindingResult(), headers, status, request);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return handleValidationInternal(ex, ex.getBindingResult(), headers, status, request);
+    }
+
     private ResponseEntity<Object> handleValidationInternal(Exception ex,
                                                             BindingResult bindingResult, HttpHeaders headers,
                                                             HttpStatus status, WebRequest request) {
@@ -249,6 +254,10 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
         return handleValidationInternal(ex, ex.getBindingResult(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
+
+
+
+
     private Problem.ProblemBuilder createProblemType (HttpStatus status, ProblemType problemType, String detail){
 
         return Problem.builder()
