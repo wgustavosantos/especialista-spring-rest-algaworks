d9387c1b6692b6c0007b9c3c5a3dd1b880443f9b
Wenderson Santos <guto15santos@gmail.com>
2023-04-01 15:42:25 -0300

    13.15. Desafio- adicionando os filtros na consulta de vendas diárias.ts

--- src/main/java/com/algaworks/algafood/infrastructure/repository/service/VendaQueryServiceImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/service/VendaQueryServiceImpl.java
@@ -3,12 +3,14 @@ package com.algaworks.algafood.infrastructure.repository.service;
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.model.Pedido;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
+import com.algaworks.algafood.domain.model.enums.StatusPedido;
 import com.algaworks.algafood.domain.service.VendaQueryService;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -25,6 +27,25 @@ public class VendaQueryServiceImpl implements VendaQueryService {
         final CriteriaQuery<VendaDiaria> query = criteriaBuilder.createQuery(VendaDiaria.class);
         final Root<Pedido> root = query.from(Pedido.class);
 
+        List<Predicate> predicates = new ArrayList<>();
+
+        if(vendaDiariaFilter == null){
+            vendaDiariaFilter = new VendaDiariaFilter();
+        }
+        if(vendaDiariaFilter.getRestauranteId() != null){
+            predicates.add(criteriaBuilder.equal(root.get("restaurante").get("id"), vendaDiariaFilter.getRestauranteId()));
+        }
+
+        if(vendaDiariaFilter.getDataCriacaoInicio() != null){
+            predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("dataCriacao"), vendaDiariaFilter.getDataCriacaoInicio()));
+        }
+
+        if(vendaDiariaFilter.getDataCriacaoFim() != null){
+            predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("dataCriacao"), vendaDiariaFilter.getDataCriacaoFim()));
+        }
+
+       predicates.add(root.get("status").in(StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
+
         final Expression<Date> function = criteriaBuilder.function("date", Date.class, root.get("dataCriacao"));
 
         final CompoundSelection<VendaDiaria> selection = criteriaBuilder.construct(VendaDiaria.class,
@@ -32,7 +53,10 @@ public class VendaQueryServiceImpl implements VendaQueryService {
                 criteriaBuilder.count(root.get("id")),
                 criteriaBuilder.sum(root.get("valorTotal")));
 
-        final CriteriaQuery<VendaDiaria> select = query.select(selection);
+
+
+        query.select(selection);
+        query.where(predicates.toArray(new Predicate[0]));
         query.groupBy(function);
 
         return entityManager.createQuery(query).getResultList();
