22b9ce4ee402ff7ab50fae7a435e51116bb92be8
Wenderson Santos <guto15santos@gmail.com>
2023-04-01 22:28:58 -0300

    13.19. Estruturando endpoint e serviço de emissão de relatório em PDF

--- src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
@@ -3,7 +3,11 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 import com.algaworks.algafood.domain.service.VendaQueryService;
+import com.algaworks.algafood.domain.service.VendaReportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,8 +22,23 @@ public class EstatisticasController {
     @Autowired
     private VendaQueryService vendaQueryService;
 
-    @GetMapping("/vendas-diarias")
-    public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter vendaDiariaFilter, @RequestParam (required = false, defaultValue = "+00:00") String timeOffSet){
+    @Autowired
+    private VendaReportService vendaReportService;
+
+    @GetMapping(path ="/vendas-diarias", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter vendaDiariaFilter,
+            @RequestParam (required = false, defaultValue = "+00:00") String timeOffSet){
         return vendaQueryService.consultarVendasDiarias(vendaDiariaFilter, timeOffSet);
+    }
+
+    @GetMapping(path ="/vendas-diarias", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<byte[]> consultarVendasDiariasPdf(VendaDiariaFilter vendaDiariaFilter,
+                                                    @RequestParam (required = false, defaultValue = "+00:00") String timeOffSet){
+        final byte[] bytesPdf = vendaReportService.emitirVendasDiarias(vendaDiariaFilter, timeOffSet);
+
+        final HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=vendas-diarias.pdf");
+
+        return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF).body(bytesPdf);
     }
 }
