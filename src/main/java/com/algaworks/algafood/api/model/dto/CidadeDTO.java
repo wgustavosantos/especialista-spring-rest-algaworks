cb6ba12ada8784f0cfcce11b1576d0b2ae3d0441
Wenderson Santos <guto15santos@gmail.com>
2023-04-10 09:02:00 -0300

    18.10. Descrevendo modelos de representações e suas propriedades

--- src/main/java/com/algaworks/algafood/api/model/dto/CidadeDTO.java
+++ src/main/java/com/algaworks/algafood/api/model/dto/CidadeDTO.java
@@ -1,12 +1,19 @@
 package com.algaworks.algafood.api.model.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+@ApiModel(value = "cidade", description = "Representa uma cidade")
 @Getter
 @Setter
 public class CidadeDTO {
+
+    @ApiModelProperty(value = "ID da cidade", example = "1")
     private Long id;
+
+    @ApiModelProperty(example = "Uberlândia")
     private String nome;
     private EstadoDTO estado;
 }
--- src/main/java/com/algaworks/algafood/api/model/inputDto/CidadeInputDTO.java
+++ src/main/java/com/algaworks/algafood/api/model/inputDto/CidadeInputDTO.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.api.model.inputDto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,13 +9,16 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+@ApiModel(value = "Representa uma cidade")
 @Getter
 @Setter
 public class CidadeInputDTO {
 
+    @ApiModelProperty(example = "Uberlândia")
     @NotBlank(message = "Não é permitido um campo vazio")
     private String nome;
 
+    @ApiModelProperty(example = "1")
     @NotNull
     @Valid
     private EstadoIdInputDTO estado;
