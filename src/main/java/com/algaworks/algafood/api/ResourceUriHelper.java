ab55aa216d131ab2545c6b841de7354253e8023b
Wenderson Santos <guto15santos@gmail.com>
2023-04-12 15:15:02 -0300

    19.2. Adicionando a URI do recurso criado no header da resposta

---
+++ src/main/java/com/algaworks/algafood/api/ResourceUriHelper.java
@@ -0,0 +1,33 @@
+package com.algaworks.algafood.api;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+
+@UtilityClass
+public class ResourceUriHelper {
+
+    public static void addUriInResponseHeader(Object resourceId){
+
+        /*Monta uma uri vinda da request. URI do recurso criado*/
+        final URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{cidadeId}")
+                .buildAndExpand(resourceId).toUri();
+
+            /*Classe Holder para expor a solicitação da Web na forma de um objeto
+             RequestAttributes associado a thread*/
+        final ServletRequestAttributes requestAttributes =
+                ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes());
+
+        final HttpServletResponse response = requestAttributes.getResponse();
+
+        /*Header enviado na resposta com a uri*/
+        response.addHeader(HttpHeaders.LOCATION, uri.toString());
+
+    }
+}
--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -1,11 +1,11 @@
 package com.algaworks.algafood.api.controller;
 
+import com.algaworks.algafood.api.ResourceUriHelper;
 import com.algaworks.algafood.api.assembler.CidadeAssembler;
 import com.algaworks.algafood.api.assembler.CidadeInputDisassembler;
-import com.algaworks.algafood.api.openapi.controller.CidadeControllerOpenApi;
-import com.algaworks.algafood.api.exceptionhandler.Problem;
 import com.algaworks.algafood.api.model.dto.CidadeDTO;
 import com.algaworks.algafood.api.model.inputDto.CidadeInputDTO;
+import com.algaworks.algafood.api.openapi.controller.CidadeControllerOpenApi;
 import com.algaworks.algafood.domain.exception.EstadoNaoEncontradoException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Cidade;
@@ -13,12 +13,6 @@ import com.algaworks.algafood.domain.model.Estado;
 import com.algaworks.algafood.domain.service.CidadeService;
 import com.algaworks.algafood.domain.service.EstadoService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -46,22 +40,21 @@ public class CidadeController implements CidadeControllerOpenApi {
     private CidadeInputDisassembler cidadeInputDisassembler;
 
     @Override
-    @ApiOperation("Cadastra uma cidade")
     @PostMapping
-    @ApiResponse(responseCode = "201", description = "Cidade cadastrada")
-    public ResponseEntity<CidadeDTO> adicionar(@ApiParam(name = "corpo", value = "Representação de uma nova cidade")
-                                               @RequestBody @Valid CidadeInputDTO cidadeInputDTO) {
+    public ResponseEntity<CidadeDTO> adicionar(@RequestBody @Valid CidadeInputDTO cidadeInputDTO) {
         try {
             final Cidade cidade = cidadeInputDisassembler.DTOtoDomainModel(cidadeInputDTO);
-            Cidade c = cidadeService.salvar(cidade);
-            return ResponseEntity.status(HttpStatus.CREATED).body(cAssembler.toDTO(c));
+            final CidadeDTO cidadeSalva = cAssembler.toDTO(cidadeService.salvar(cidade));
+
+            ResourceUriHelper.addUriInResponseHeader(cidadeSalva.getId());
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(cidadeSalva);
         } catch (EstadoNaoEncontradoException e) {
             throw new NegocioException(e.getMessage(), e);
         }
     }
 
     @Override
-    @ApiOperation("Lista as cidades")
     @GetMapping
     public ResponseEntity<List<CidadeDTO>> listar() {
         final List<Cidade> cidades = cidadeService.listar();
@@ -69,24 +62,14 @@ public class CidadeController implements CidadeControllerOpenApi {
     }
 
     @Override
-    @ApiOperation("Busca uma cidade por ID")
-    @ApiResponses({
-            @ApiResponse(responseCode = "400", description = "ID da cidade inválido", content = @Content(schema = @Schema(implementation = Problem.class))),
-            @ApiResponse(responseCode = "404", description = "Cidade não encontrada", content = @Content(schema = @Schema(implementation = Problem.class)))
-    })
     @GetMapping("/{cidadeId}")
-    public CidadeDTO buscar(@ApiParam("ID de uma cidade") @PathVariable Long cidadeId) {
+    public CidadeDTO buscar(@PathVariable Long cidadeId) {
         return cAssembler.toDTO(cidadeService.buscar(cidadeId));
     }
 
     @Override
-    @ApiOperation("Atualiza uma cidade por ID")
     @PutMapping("/{cidadeId}")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Cidade atualizada"),
-            @ApiResponse(responseCode = "404", description = "Cidade não encontrada", content = @Content(schema = @Schema(implementation = Problem.class)))
-    })
-    public CidadeDTO atualizar(@RequestBody @Valid CidadeInputDTO cidadeInputDTO, @ApiParam("ID de uma cidade") @PathVariable Long cidadeId) {
+    public CidadeDTO atualizar(@RequestBody @Valid CidadeInputDTO cidadeInputDTO, @PathVariable Long cidadeId) {
         Cidade cidadeAtual;
         try {
             Estado estadoAtual = estadoService.buscar(cidadeInputDTO.getEstado().getId());
@@ -100,14 +83,9 @@ public class CidadeController implements CidadeControllerOpenApi {
     }
 
     @Override
-    @ApiResponses({
-            @ApiResponse(responseCode = "204", description = "Cidade excluída"),
-            @ApiResponse(responseCode = "404", description = "Cidade não encontrada", content = @Content(schema = @Schema(implementation = Problem.class)))
-    })
-    @ApiOperation("Exclui uma cidade por ID")
     @DeleteMapping("/{cidadeId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deletar(@ApiParam("ID de uma cidade") @PathVariable Long cidadeId) {
+    public void deletar(@PathVariable Long cidadeId) {
         cidadeService.deletar(cidadeId);
 
     }
