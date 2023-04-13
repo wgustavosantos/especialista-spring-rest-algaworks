f8f72a2a425e2e0e4209a1716f3ee012eec3ef66
Wenderson Santos <guto15santos@gmail.com>
2023-04-13 09:23:16 -0300

    19.11. Montando modelo de representação com RepresentationModelAssembler

--- src/main/java/com/algaworks/algafood/api/assembler/CidadeAssembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/CidadeAssembler.java
@@ -1,24 +1,69 @@
 package com.algaworks.algafood.api.assembler;
 
+import com.algaworks.algafood.api.controller.CidadeController;
+import com.algaworks.algafood.api.controller.EstadoController;
 import com.algaworks.algafood.api.model.dto.CidadeDTO;
 import com.algaworks.algafood.domain.model.Cidade;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
-public class CidadeAssembler {
+public class CidadeAssembler extends RepresentationModelAssemblerSupport<Cidade, CidadeDTO> {
 
     @Autowired
     ModelMapper modelMapper;
 
-    public CidadeDTO toDTO(Cidade cidade){
-        return modelMapper.map(cidade, CidadeDTO.class);
+    public CidadeAssembler(){
+        super(CidadeController.class, CidadeDTO.class);
     }
 
-    public List<CidadeDTO> toListDTO(List<Cidade> cidades){
-        return cidades.stream().map(this::toDTO).toList();
+    @Override
+    public CidadeDTO toModel(Cidade cidade){
+
+
+        /*_link.self para o próprio recurso
+        cidadeDTO.add(linkTo(CidadeController.class)
+                .slash(cidadeDTO.getId()).withSelfRel());*/
+
+        /*_link.self para o próprio recurso
+        linkTo recebendo um Object method como argumento
+        chamada de método para controller específicos
+        final Link linkCidade = linkTo( methodOn(CidadeController.class).buscar(cidadeDTO.getId()) ).withSelfRel();
+        cidadeDTO.add(linkCidade);*/
+
+        /*Adicionando linkself dinamicamente*/
+        final CidadeDTO cidadeDTO = createModelWithId(cidade.getId(), cidade);
+
+        modelMapper.map(cidade, cidadeDTO);
+
+        /*_link.collection para coleção de cidades
+        cidadeDTO.add(linkTo(CidadeController.class)
+                .withRel("cidades"));*/
+
+        final Link linkCidades = linkTo(methodOn(CidadeController.class).listar()).withRel("Cidades");
+        cidadeDTO.add(linkCidades);
+
+        /*_link.self para o recurso estado/id dentro de cidadeDTO
+        cidadeDTO.getEstado().add(linkTo(EstadoController.class)
+                .slash(cidadeDTO.getEstado().getId()).withSelfRel());*/
+
+        final Link linkEstados = linkTo(methodOn(EstadoController.class).
+                buscar(cidadeDTO.getEstado().getId())).withSelfRel();
+        cidadeDTO.getEstado().add(linkEstados);
+
+        return cidadeDTO;
+    }
+
+    @Override
+    public CollectionModel<CidadeDTO> toCollectionModel(Iterable<? extends Cidade> entities) {
+
+        return super.toCollectionModel(entities).add(linkTo(CidadeController.class).withSelfRel());
     }
 }
--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -15,7 +15,6 @@ import com.algaworks.algafood.domain.service.EstadoService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -23,9 +22,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Api(tags = "Cidades")
 @RestController
@@ -49,7 +45,7 @@ public class CidadeController implements CidadeControllerOpenApi {
     public ResponseEntity<CidadeDTO> adicionar(@RequestBody @Valid CidadeInputDTO cidadeInputDTO) {
         try {
             final Cidade cidade = cidadeInputDisassembler.DTOtoDomainModel(cidadeInputDTO);
-            final CidadeDTO cidadeSalva = cAssembler.toDTO(cidadeService.salvar(cidade));
+            final CidadeDTO cidadeSalva = cAssembler.toModel(cidadeService.salvar(cidade));
 
             ResourceUriHelper.addUriInResponseHeader(cidadeSalva.getId());
 
@@ -63,62 +59,15 @@ public class CidadeController implements CidadeControllerOpenApi {
     @GetMapping
     public CollectionModel<CidadeDTO> listar() {
         final List<Cidade> cidades = cidadeService.listar();
-        final List<CidadeDTO> cidadesDTOS = cAssembler.toListDTO(cidades);
 
-        /*Para criar links para cada objeto json*/
-        cidadesDTOS.forEach(cidade -> {
-            final Link linkEstados = linkTo(methodOn(EstadoController.class).
-                    buscar(cidade.getEstado().getId())).withSelfRel();
-            cidade.getEstado().add(linkEstados);
-
-            final Link linkCidades = linkTo(methodOn(CidadeController.class).listar()).withRel("Cidades");
-            cidade.add(linkCidades);
-
-            final Link linkCidade = linkTo( methodOn(CidadeController.class).buscar(cidade.getId()) ).withSelfRel();
-            cidade.add(linkCidade);
-        });
-
-        final CollectionModel<CidadeDTO> cidadesCollectionModel = CollectionModel.of(cidadesDTOS);
-
-        /*link para listagem de cidades*/
-        cidadesCollectionModel.add(linkTo(CidadeController.class).withSelfRel());
-
-
-
-        return cidadesCollectionModel;
+        return cAssembler.toCollectionModel(cidades);
     }
 
     @Override
     @GetMapping("/{cidadeId}")
     public CidadeDTO buscar(@PathVariable Long cidadeId) {
-        final CidadeDTO cidade = cAssembler.toDTO(cidadeService.buscar(cidadeId));
 
-        /*_link.self para o próprio recurso
-        cidade.add(linkTo(CidadeController.class)
-                .slash(cidade.getId()).withSelfRel());*/
-
-        /*_link.self para o próprio recurso
-        linkTo recebendo um Object method como argumento
-        chamada de método para controller específicos*/
-        final Link linkCidade = linkTo( methodOn(CidadeController.class).buscar(cidade.getId()) ).withSelfRel();
-        cidade.add(linkCidade);
-
-        /*_link.collection para coleção de cidades
-        cidade.add(linkTo(CidadeController.class)
-                .withRel("cidades"));*/
-
-        final Link linkCidades = linkTo(methodOn(CidadeController.class).listar()).withRel("Cidades");
-        cidade.add(linkCidades);
-
-        /*_link.self para o recurso estado/id dentro de cidade
-        cidade.getEstado().add(linkTo(EstadoController.class)
-                .slash(cidade.getEstado().getId()).withSelfRel());*/
-
-        final Link linkEstados = linkTo(methodOn(EstadoController.class).
-                buscar(cidade.getEstado().getId())).withSelfRel();
-        cidade.getEstado().add(linkEstados);
-
-        return cidade;
+        return cAssembler.toModel(cidadeService.buscar(cidadeId));
     }
 
     @Override
@@ -133,7 +82,7 @@ public class CidadeController implements CidadeControllerOpenApi {
         } catch (EstadoNaoEncontradoException e) {
             throw new NegocioException(e.getMessage(), e);
         }
-        return cAssembler.toDTO(cidadeService.atualizar(cidadeAtual));
+        return cAssembler.toModel(cidadeService.atualizar(cidadeAtual));
     }
 
     @Override
