6ca55e5d275abcfca7a7abdd2534696627dccdef
Wenderson Santos <77124683+wgustavosantos@users.noreply.github.com>
2023-05-10 11:11:46 -0300

    Update CorsConfig.java

--- algafood-auth/src/main/java/com/algaworks/algafood/auth/CorsConfig.java
+++ algafood-auth/src/main/java/com/algaworks/algafood/auth/CorsConfig.java
@@ -1,2 +1,34 @@
-package com.algaworks.algafood.auth;public class CorsConfig {
+package com.algaworks.algafood.auth;// Fonte: https://spring.io/blog/2015/06/08/cors-support-in-spring-framework#filter-based-cors-support
+
+import java.util.Collections;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.setAllowedOriginPatterns(Collections.singletonList("http://**"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/oauth/token", config);
+
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        return bean;
+    }
+
 }
