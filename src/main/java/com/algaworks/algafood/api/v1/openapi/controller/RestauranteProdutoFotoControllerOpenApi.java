b08465f08d243da90fa7e8bf1cb79564df2920ed
Wenderson Santos <guto15santos@gmail.com>
2023-06-14 20:45:52 -0300

    26.22. Descrevendo media type da resposta nos endpoints

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/RestauranteProdutoFotoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/RestauranteProdutoFotoControllerOpenApi.java
@@ -2,6 +2,10 @@ package com.algaworks.algafood.api.v1.openapi.controller;
 
 import com.algaworks.algafood.api.v1.model.dto.FotoProdutoDTO;
 import com.algaworks.algafood.api.v1.model.inputDto.FotoProdutoInput;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
@@ -16,6 +20,15 @@ public interface RestauranteProdutoFotoControllerOpenApi {
 
     void deletarFotoProduto(Long restauranteId, Long produtoId);
 
+    @Operation(hidden = true)
     ResponseEntity<?> servirFoto(Long restauranteId, Long produtoId, String acceptHeader) throws HttpMediaTypeNotAcceptableException;
+    
+    @Operation(summary = "Busca a foto do produto de um restaurante", responses = {
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = FotoProdutoModel.class)),
+                    @Content(mediaType = "image/jpeg", schema = @Schema(type = "string", format = "binary")),
+                    @Content(mediaType = "image/png", schema = @Schema(type = "string", format = "binary"))
+            })
+    })
     FotoProdutoDTO buscar(Long restauranteId, Long produtoId);
 }
