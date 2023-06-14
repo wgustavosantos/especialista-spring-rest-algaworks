19607a700fc4bfcc26c7a84b0f836e39ee135097
Wenderson Santos <guto15santos@gmail.com>
2023-06-14 14:56:18 -0300

    26.14. Descrevendo o modelo de representação de problema

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.exceptionhandler;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,29 +12,38 @@ import java.util.List;
 @Getter
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Schema(name = "Problema")
 public class Problem {
 
+    @Schema(example = "400")
     private Integer status;
 
+    @Schema(example = "2022-07-15T11:21:50.902245498Z")
     private OffsetDateTime timeStamp;
 
     private String type;
 
+    @Schema(example = "https://algafood.com.br/dados-invalidos")
     private String title;
 
+    @Schema(example = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.")
     private String detail;
 
-
+    @Schema(example = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.")
     private String userMessage;
 
+    @Schema(description = "Lista de objetos ou campos que geraram o erro")
     private List<Object> objects;
 
     @Getter
     @Builder
+    @Schema(name = "ObjetoProblema")
     public static class Object {
 
+        @Schema(example = "preco")
         private String name;
 
+        @Schema(example = "O preço é inválido")
         private String userMessage;
     }
 }
--- src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
+++ src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
@@ -1,14 +1,18 @@
 package com.algaworks.algafood.core.springdoc;
 
+import com.algaworks.algafood.api.exceptionhandler.Problem;
+import io.swagger.v3.core.converter.ModelConverters;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.OAuthFlow;
 import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import io.swagger.v3.oas.annotations.security.OAuthScope;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.tags.Tag;
@@ -17,6 +21,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @SecurityScheme(name = "security_auth",
@@ -47,6 +53,8 @@ public class SpringDocConfig {
                         .url("https://algaworks.com")
                 ).tags(Arrays.asList(
                         new Tag().name("Cidades").description("Gerencia as cidades")
+                )).components(new Components().schemas(
+                        gerarSchemas()
                 ));
     }
 
@@ -85,6 +93,18 @@ public class SpringDocConfig {
                     })
             );
         };
+    }
+
+    private Map<String, Schema> gerarSchemas() {
+        final Map<String, Schema> schemaMap = new HashMap<>();
+
+        Map<String, Schema> problemSchema = ModelConverters.getInstance().read(Problem.class);
+        Map<String, Schema> problemObjectSchema = ModelConverters.getInstance().read(Problem.Object.class);
+
+        schemaMap.putAll(problemSchema);
+        schemaMap.putAll(problemObjectSchema);
+
+        return schemaMap;
     }
 
 }
